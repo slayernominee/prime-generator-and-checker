497303663b2eefd0906d09acf5c13f83544cee03
slayernominee <slayernominee_git@proton.me>
2023-02-06 18:42:07 +0100

    code via arguments

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,76 @@
-use std::io;
+//use std::io;
+use std::env;
 
 fn main() {
 
-    let my_number: u64 = 73;
-    
-    // * values my_number is dividable
-    let dividable_by: Vec<u64> = get_dividable_by(my_number);
-    println!("{my_number} is dividable by {:?}", dividable_by);
-    
-    // * check if its a prim
-    let prim = is_prim(my_number);
-    if prim {
-        println!("so its a prim");
-    } else {
-        println!("its no prim");
+    let args: Vec<String> = env::args().collect();
+
+    let rescue_mode: String = String::from("--help");
+
+    let mut mode: &String = &rescue_mode;
+
+    if args.len() >= 2 {
+        mode = &args[1];
+
+        if mode == "--gen" && args.len() < 4 {
+            println!("not enought arguments!");
+            mode = &rescue_mode;
+        } else if mode == "--check" && args.len() < 3 {
+            println!("not enought arguments!");
+            mode = &rescue_mode;
+        }
     }
     
+        
 
-    // * gen prims in range
+    // valid modes:
+    // --gen, --check, --help
 
-    let mut start = String::new();
-    let mut end = String::new();
+    if mode == "--gen" {
 
-    println!("Where should the Prim Generator Start?");
-    io::stdin().read_line(&mut start)
-        .expect("couldnt read line");
+        let start: u64 = args[2].trim().parse().expect("no valid number on argument 2");
+        let end: u64 = args[3].trim().parse().expect("no valid number on argument 3");
+
+        /*
+        let mut start = String::new();
+        let mut end = String::new();
+
+        println!("Where should the Prim Generator Start?");
+        io::stdin().read_line(&mut start)
+            .expect("couldnt read line");
+        
+        let start: u64 = start.trim().parse().expect("no valid number");
+
+        println!("Where should the Prim Generator Stop?");
+        io::stdin().read_line(&mut end)
+            .expect("couldnt read line");
+
+        let end: u64 = end.trim().parse().expect("no valid number");
+        
+         */
+
+        println!("Prims between {start} and {end}: ");
+        let prims: Vec<u64> = gen_prims(start, end);
+        println!("{:?}", prims);
+    } else if mode == "--check" {
+        let my_number: u64 = args[2].trim().parse().expect("no valid number on argument 2");
     
-    let start: u64 = start.trim().parse().expect("no valid number");
+        let prim = is_prim(my_number);
+        if prim {
+            println!("{my_number} is a prim number");
+        } else {
+            println!("{my_number} is no prim number!");
 
-    println!("Where should the Prim Generator Stop?");
-    io::stdin().read_line(&mut end)
-        .expect("couldnt read line");
-
-    let end: u64 = end.trim().parse().expect("no valid number");
-    
-    println!("Prims between {start} and {end}: ");
-    let prims: Vec<u64> = gen_prims(start, end);
-    println!("{:?}", prims);
+            let dividable_by: Vec<u64> = get_dividable_by(my_number);
+            println!("Because {my_number} is dividable by {:?}", dividable_by);
+            println!("But a prime number needs to be only dividable by 1 and itself!");
+        }
+        
+    } else {
+        println!("-----------------------------------------------------------------");
+        println!("Help Page");
+        println!("Please use the arguments: --gen <start> <end> or --check <number>");
+    }
 }
 
 fn is_prim(number: u64) -> bool {
